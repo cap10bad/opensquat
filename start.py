78a058a3a8524718775ca5c96283dc9abb8ed03d
Captain Bad <cba@freshdrops.io>
2023-04-23 22:09:47 -0400

    don't insert partners each time

--- start.py
+++ start.py
@@ -23,41 +23,6 @@ except Exception as e:
 db = client["boringsec_db"]
 collection = db["phrases"]
 
-# data = collection.find()
-
-
-# partnersfile = 'partners.txt'
-# with open(partnersfile, "w") as f:
-#   partnerslist = f.write(str(document) + "\n")
-
-# Read the data from partners.txt and insert to MongoDB collection
-
-partnersfile = 'partners.txt'
-with open(partnersfile, "r") as p:
-    partners = p.readlines()
-
-partner=''
-keywords=[]
-
-for partnerline in partners:
-    if(partner == ''):
-        partner = partnerline.strip()
-        keywords = [partner]
-        continue
-    
-    if(partnerline[0] in whitespace):
-        keywords.append(partnerline)
-        continue
-
-    if partner and keywords:
-        collection.insert_one({"partner": partner, "keywords": keywords})
-        keywords = []
-    
-    partner = partnerline.strip()
-
-    if partner and keywords:
-        collection.insert_one({"partner": partner, "keywords": keywords})
-
 message = ''
 datestring = datetime.datetime.utcnow().strftime("%y-%m-%d")
 
@@ -77,10 +42,6 @@ for document in collection.find():
     
     if(len(lines) > 0):
         message = f'{message}❗**Suspicious links for the {partner} community**❗\n{"".join(lines)}\n'
-
-    #partner = ''
-    #keywords = []
-
 
 if(len(message) > 0):
     message = f'{message}For more information on what to do if your community has domains in this list, go here: https://discord.com/channels/933253328794689546/1073264004614594641/1073304829105016843'
