c6aaa40ed3b19ee131df0fe14de5346528e4205d
Captain Bad <cba@freshdrops.io>
2023-04-25 21:00:01 -0400

    remove phrase and partner, correct keywords array not phrases

--- cogs/phrases.py
+++ cogs/phrases.py
@@ -38,15 +38,57 @@ class Phrases(commands.Cog):
 
         if partner is None:
             await ctx.send(f"Adding partner *{splits[0]}*")
-            partner = {"partner": splits[0], "phrases": []}
+            partner = {"partner": splits[0], "keywords": []}
             collection.insert_one(partner)
 
-        if splits[1] in partner["phrases"] or splits[1] == splits[0]:
+        if splits[1] in partner["keywords"] or splits[1] == splits[0]:
             await ctx.send(
                 f"Phrase *{splits[1]}* already exists for partner *{splits[0]}*")
         else:
-            partner["phrases"].append(splits[1])
-            new_phrases = "\n\t".join(partner["phrases"])
+            partner["keywords"].append(splits[1])
+            new_phrases = "\n\t".join(partner["keywords"])
             await ctx.send(f"Adding phrase *{splits[1]}* to partner *{splits[0]}*\nNew phrase list:\n\t{new_phrases}")
             collection.update_one({"partner": splits[0]},
-                                  {"$push": {"phrases": splits[1]}})
+                                  {"$push": {"keywords": splits[1]}})
+
+    @commands.command(pass_context=True)
+    async def remove_phrase(self, ctx, *, message=""):
+        splits = message.split()
+
+        if len(splits) != 2:
+            await ctx.send("remove_phrase *partner* *phrase*")
+            return
+
+        partner = collection.find_one({"partner": splits[0]})
+
+        if partner is None:
+            await ctx.send(f"No partner *{splits[0]}* found")
+            return
+
+        if splits[1] in partner["keywords"] or splits[1] == splits[0]:
+            partner["keywords"].remove(splits[1])
+            new_phrases = "\n\t".join(partner["keywords"])
+            await ctx.send(f"Removing phrase *{splits[1]}* from partner *{splits[0]}*\nNew phrase list:\n\t{new_phrases}")
+            collection.update_one({"partner": splits[0]},
+                                  {"$pull": {"keywords": splits[1]}})
+        else:
+            await ctx.send(
+                f"No phrase *{splits[1]}* exists for partner *{splits[0]}*")
+
+
+    @commands.command(pass_context=True)
+    async def remove_partner(self, ctx, *, message=""):
+        splits = message.split()
+
+        if len(splits) != 1:
+            await ctx.send("remove_partner *partner*")
+            return
+
+        partner = collection.find_one({"partner": splits[0]})
+
+        if partner is None:
+            await ctx.send(f"No partner *{splits[0]}* found")
+            return
+
+        collection.delete_one({"partner": splits[0]})
+        await ctx.send(f"Removing partner *{splits[0]}*")
